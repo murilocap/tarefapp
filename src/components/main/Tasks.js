7f1f583c6796997a6ba80ad3a697bfa6c0045f2e
Murilo Capita <62261536+murilocap@users.noreply.github.com>
2024-06-30 17:48:53 -0300

    Alterações mínimas de estilo

--- src/components/main/Tasks.js
+++ src/components/main/Tasks.js
@@ -4,6 +4,17 @@ import Task from "./Task";
 function Tasks() {
     return (
         <ul id="Tasks">
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ false } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ true } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ true } desc={ "Exemplo de tarefa a fazer." }/>
+            <Task finished={ true } desc={ "Exemplo de tarefa a fazer." }/>
             <Task finished={ true } desc={ "Exemplo de tarefa a fazer." }/>
         </ul>
     )
